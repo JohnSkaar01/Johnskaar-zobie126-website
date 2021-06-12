3e894b4ae85a1df6d413d78345fba1a5b7369db9
JohnSkaar01 <82280227+JohnSkaar01@users.noreply.github.com>
2021-06-12 12:58:44 -0400

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,17 @@
+{
+  "name": "Zobie / JohnSkaar website",
+  "short_name": "z and skaar",
+  "start_url": "/",
+  "display": "fullscreen",
+  "orientation": "any",
+  "theme_color": "#60a0ff",
+  "background_color": "#000000",
+  "description": "Website cool 😎",
+  "icons": [
+    {
+      "src": "",
+      "sizes": "1024x1024",
+      "type": "image/png"
+    }
+  ]
+}
